51891b3687ca737b121f6d5f26e000f140ca8413
Pawel Idziak <pa.idziak@gmail.com>
2020-01-03 22:47:55 +0100

    feat(float btn, goals): change float btn output name and use it in goals component

--- src/app/shared/components/float-btn/float-btn.component.ts
+++ src/app/shared/components/float-btn/float-btn.component.ts
@@ -9,7 +9,7 @@ export class FloatBtnComponent implements OnInit {
   @Input() size: number;
   @Input() bgColor = '#3F51B5';
   @Input() fontColor = '#FAFAFA';
-  @Output() tap: EventEmitter<any> = new EventEmitter<any>();
+  @Output() fabTap: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
@@ -17,7 +17,7 @@ export class FloatBtnComponent implements OnInit {
   }
 
   onTap(event: any) {
-    console.log('tap fab')
+    this.fabTap.emit(event);
   }
 
 }
