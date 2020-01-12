93fc06d17c82ce9584ba816cbef6e81ef35a6eac
Pawel Idziak <pa.idziak@gmail.com>
2020-01-12 19:01:41 +0100

    feat(priority): get data from store

--- src/app/modules/goal-dropdowns/priority/priority.component.ts
+++ src/app/modules/goal-dropdowns/priority/priority.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Priority } from '../../add-goal/+state/add-goal.models';
+import { GoalDropdownsBase } from '../goal-dropdowns.base';
+import { AddGoalFacade } from '../../add-goal/+state/add-goal.facade';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-priority',
@@ -6,18 +11,31 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./priority.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PriorityComponent implements OnInit {
-  priority = {
-    icon: 'mdi-priority-high',
-    iconColors: ['#F44336', '#FF9800', '#2196F3', '#9E9E9E'],
-    items: ['A - High', 'B - Medium', 'C - Low', 'D - None']
-  };
+export class PriorityComponent extends GoalDropdownsBase implements OnInit {
+  icon = 'mdi-priority-high';
+  iconColors = ['#F44336', '#FF9800', '#2196F3', '#9E9E9E'];
 
-  constructor() {}
+  priorities: Priority[];
+  items$: Observable<string[]> = this.addGoalFacade.priorities$.pipe(
+    map(priorities => priorities.map(prority => prority.name))
+  );
 
-  ngOnInit(): void {}
-
-  onItemChanged(index: number) {
-    console.log('onItemChanged', index);
+  constructor(private addGoalFacade: AddGoalFacade) {
+    super();
   }
+
+  ngOnInit() {
+    this.getProrities();
+  }
+
+  getProrities() {
+    this.unsubscribes$.add(
+      this.addGoalFacade.priorities$.subscribe(
+        (prorities: Priority[]) => (this.priorities = prorities)
+      )
+    );
+  }
+
+  onItemChanged = (index: number) =>
+    this.addGoalFacade.selecPriority(this.priorities[index]);
 }
