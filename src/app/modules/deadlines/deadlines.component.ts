d16332d5e6926caa22bf5babe51fff310891b457
Pawel Idziak <pa.idziak@gmail.com>
2020-01-12 11:35:53 +0100

    feat(deadlines): add deadlines component

---
+++ src/app/modules/deadlines/deadlines.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-deadlines',
+  templateUrl: './deadlines.component.html',
+  styleUrls: ['./deadlines.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DeadlinesComponent implements OnInit {
+  deadline = {
+    icon: 'mdi-date-range',
+    iconColors: ['#3F51B5', '#009688', '#673AB7', '#795548'],
+    items: ['Today', 'Tomorrow', 'Long-term', 'Todo']
+  };
+  
+  constructor() {}
+
+  ngOnInit(): void {}
+}
