3b517c292a73cc2f9e9e62f74dc2d526438acd62
pawel.idziak <pawel.idziak@teleste.com>
2020-01-13 17:51:36 +0100

    feat(goals list): get data from store

--- src/app/modules/player/players.component.ts
+++ src/app/modules/player/players.component.ts
@@ -1,18 +1,33 @@
+import { GoalsListFacade } from './+state/goals-list.facade';
 import { Component, OnInit } from '@angular/core';
 import { DataService, DataItem } from '../data.service';
+import { Goal } from './+state/goals-list.models';
+import { Observable } from 'rxjs';
 
 @Component({
-    selector: "ns-players",
-    moduleId: module.id,
-    templateUrl: './players.component.html',
-    styleUrls: ['./players.component.scss']
+  selector: 'ns-players',
+  moduleId: module.id,
+  templateUrl: './players.component.html',
+  styleUrls: ['./players.component.scss']
 })
 export class PlayerComponent implements OnInit {
-    items: DataItem[];
+  items: DataItem[];
 
-    constructor(private itemService: DataService) { }
+  loaded$: Observable<boolean>;
+  goals$: Observable<Goal[]>;
 
-    ngOnInit(): void {
-        this.items = this.itemService.getPlayers();
-    }
+  constructor(
+    private itemService: DataService,
+    private facade: GoalsListFacade
+  ) {}
+
+  ngOnInit(): void {
+    this.items = this.itemService.getPlayers();
+    // console.log('this.items', this.items);
+    this.loaded$ = this.facade.loaded$;
+    this.goals$ = this.facade.allGoalsList$;
+
+    this.loaded$.subscribe(res => console.log('loaded$', res));
+    this.goals$.subscribe(res => console.log('goals$', res));
+  }
 }
