eb2ce7e1591657e25887b4c26ebb3079754eba6f
Pawel Idziak <pa.idziak@gmail.com>
2020-01-19 20:24:57 +0100

    feat(goals): generate mock goals

--- src/app/core/pages/goals/services/goals.service.ts
+++ src/app/core/pages/goals/services/goals.service.ts
@@ -3,39 +3,37 @@ import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
 import { Observable } from 'rxjs';
 
-const GOALS_LIST: Goal[] = [
-  {
-    id: '1',
-    name: 'Name 1',
-    desc: 'Desc 1',
-    deadline: 'Deadline 1',
-    priority: { id: '2', name: 'B - Medium', tag: 'B', color: '#FF9800' },
-    repeat: 'Repeat 1',
-    done: false
-  },
-  {
-    id: '2',
-    name: 'Name 2',
-    desc: 'Desc 2',
-    deadline: 'Deadline 2',
-    priority: { id: '3', name: 'C - Low', tag: 'C', color: '#2196F3' },
-    repeat: 'Repeat 2',
-    done: true
-  },
-  {
-    id: '3',
-    name: 'Name 3',
-    desc: 'Desc 3',
-    deadline: 'Deadline 3',
-    priority: { id: '1', name: 'A - High', tag: 'A', color: '#F44336' },
-    repeat: 'Repeat 3',
-    done: false
-  }
-];
-
 @Injectable()
 export class GoalsService {
   getAll(): Observable<Goal[]> {
-    return of(GOALS_LIST);
+    return of(this.generateMockGoals(20));
+  }
+
+  private generateMockGoals(quantity: number): Goal[] {
+    const res: Goal[] = [];
+    for (let i = 1; i <= quantity; i++) {
+      res.push({
+        id: `${i}`,
+        name: `Name ${i}`,
+        desc: `Desc ${i}`,
+        deadline: `Deadline ${i}`,
+        repeat: `Repeat ${i}`,
+        done: Math.random() >= 0.5,
+        priority: this.getRandomPriority(i)
+      });
+    }
+
+    return res;
+  }
+
+  private getRandomPriority(id: number) {
+    const priorities = [
+      { id: id, name: 'A - High', tag: 'A', color: '#F44336' },
+      { id: id, name: 'B - Medium', tag: 'B', color: '#FF9800' },
+      { id: id, name: 'C - Low', tag: 'C', color: '#2196F3' },
+      { id: id, name: 'C - None', tag: 'D', color: '#9E9E9E' }
+    ];
+
+    return priorities[Math.floor(priorities.length * Math.random())];
   }
 }
