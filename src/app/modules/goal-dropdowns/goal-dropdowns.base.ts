e48a954b78f1d24ef4839ca2cd5ca0c80c594920
pawel.idziak <pawel.idziak@teleste.com>
2020-02-17 17:39:15 +0100

    feat(dropdowns): create base class and facade interface

--- src/app/modules/goal-dropdowns/goal-dropdowns.base.ts
+++ src/app/modules/goal-dropdowns/goal-dropdowns.base.ts
@@ -1,10 +1,27 @@
-import { OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
 
-export class GoalDropdownsBase implements OnDestroy {
-  protected unsubscribes$ = new Subscription();
+import { OnInit, Input } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { DropdownsFacade, itemType } from './dropdowns-facade';
 
-  ngOnDestroy(): void {
-    this.unsubscribes$.unsubscribe();
+
+export class GoalDropdownsBase implements OnInit {
+  items$ = this.facade.items$;
+  selectedIndex$: Observable<number> = of(0);
+
+  @Input()
+  set selectedItem(item: itemType) {
+    if (item) {
+      this.selectedIndex$ = this.facade.getItemIndex(item);
+    }
+  }
+
+  constructor(private facade: DropdownsFacade) {}
+
+  ngOnInit() {
+    this.facade.loadAll();
+  }
+
+  onItemChanged(item: itemType) {
+    this.facade.selectItem(item.id);
   }
 }
